ffc4abc12b5a7dceca45358df55e002edaa8c55d
Davidiniooo <davidinioooo@gmail.com>
2020-05-15 19:15:36 +0200

    added comments

--- CFramework.hpp
+++ CFramework.hpp
@@ -4,13 +4,14 @@
 #ifndef CFRAMEWORK_HPP
 #define CFRAMEWORK_HPP
 
+//Framework that handles graphics and other basic functionalities
 class CFramework
 {
-
 public:
-  void renderSprite(sf::Sprite &sprite);
-  void display();
-  CFramework();
+  void renderSprite(sf::Sprite &sprite);  //takes a sprite and renders it
+  void display();                         //displays all graphics that got rendered in last tick
+  CFramework();                           //constructor
+
 private:
   sf::RenderWindow window;
 };
